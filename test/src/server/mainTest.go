9df5ed73e98bfa3b7cc1fa4d3d6b52ba55b900b3
CrossRun <rujiang_g@qq.com>
2014-09-30 14:04:15 +0800

    css

--- test/src/server/mainTest.go
+++ test/src/server/mainTest.go
@@ -6,13 +6,18 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	// "server/ser"
-	// "time"
+	"time"
 )
 
 type iHandler struct {
 }
+
+const (
+	ListDir = 0x0001
+)
 
 func main() {
 
@@ -24,17 +29,23 @@ func main() {
 	// 	WriteTimeout:   10 * time.Second,
 	// 	MaxHeaderBytes: 1 << 20,
 	// }
-	http.NewServeMux().HandleFunc("/css", func(rw http.ResponseWriter, req *http.Request) {
-		file := "./css" + req.URL.Path[len("/css")-1:]
-		// if(0 & 0x0001)==0{
-		// 	if
-		// }
-		http.ServeFile(rw, req, file)
-	})
+	// http.NewServeMux().HandleFunc("/css", func(rw http.ResponseWriter, req *http.Request) {
+	// 	file := "./css" + req.URL.Path[len("/css")-1:]
+	// 	// if(0 & 0x0001)==0{
+	// 	// 	if
+	// 	// }
+	// 	http.ServeFile(rw, req, file)
+	// })
+	mux := http.NewServeMux()
+	staticDirHandler(mux, "/css/", "./css", 0)
+	// mux.HandleFunc("/css/", func(rw http.ResponseWriter, req *http.Request) {
+	// 	fmt.Println("css")
+	// })
 
-	http.HandleFunc("/test", myHandler)
-	http.HandleFunc("/nav", navHandler)
-	log.Fatal(http.ListenAndServe(":9393", nil))
+	mux.HandleFunc("/test", myHandler)
+	mux.HandleFunc("/nav", navHandler)
+	mux.HandleFunc("/exam", examHandler)
+	log.Fatal(http.ListenAndServe(":9393", mux))
 	// log.Fatal(s.ListenAndServe())
 }
 
@@ -44,6 +55,7 @@ func myHandler(rw http.ResponseWriter, req *http.Request) {
 	// 解析参数, 默认是不会解析的
 	// log.Fatalln(req.ParseForm())
 	fmt.Printf("req.ParseForm(): %v\n", req.ParseForm())
+	fmt.Println(time.Now().String() + "  " + req.URL.Path)
 	// 这些信息是输出到服务器端的打印信息
 	// log.Fatalln("request map:", req.Form)
 	// log.Fatalln("path", req.URL.Path)
@@ -66,12 +78,47 @@ func (*iHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 func navHandler(rw http.ResponseWriter, req *http.Request) {
 	// http.Redirect(rw, req, "/Temaplate/index.html", http.StatusFound)
-	t, err := template.ParseFiles("index.html")
-	fmt.Println(req.URL)
+	t, err := template.ParseFiles("Template/index.html")
+	fmt.Println(time.Now().String() + "  " + req.URL.Path)
 
 	if err == nil {
 		t.Execute(rw, nil)
 	} else {
 		fmt.Println(err.Error())
 	}
+}
+
+func examHandler(rw http.ResponseWriter, req *http.Request) {
+	// http.Redirect(rw, req, "/Temaplate/index.html", http.StatusFound)
+	t, err := template.ParseFiles("Template/example.html")
+	fmt.Println(time.Now().String() + "  " + req.URL.Path)
+
+	if err == nil {
+		t.Execute(rw, nil)
+	} else {
+		fmt.Println(err.Error())
+	}
+}
+
+func staticDirHandler(mux *http.ServeMux, prefix string, staticDir string, flags int) {
+	fmt.Println("mux")
+	mux.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+		file := staticDir + r.URL.Path[len(prefix)-1:]
+		fmt.Println(file)
+		if (flags & ListDir) == 0 {
+			if exists := isExists(file); !exists {
+				http.NotFound(w, r)
+				return
+			}
+		}
+		http.ServeFile(w, r, file)
+	})
+}
+
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
 }
